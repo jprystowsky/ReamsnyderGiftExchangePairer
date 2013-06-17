3b2651f93944957de2c59c6efeee999984c27c30
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2013-06-17 10:16:27 -0400

    Updating tests to use the new factory method. (TDD FTW.)

--- test/io/mapping/apps/reamsnydergifts/ConcretePersonTest.java
+++ test/io/mapping/apps/reamsnydergifts/ConcretePersonTest.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ConcretePersonTest {
-	
+
 	@Test
 	public void testPartnerMutually() {
 		ConcretePerson personOne = new ConcretePerson();
 		ConcretePerson personTwo = new ConcretePerson();
-		
+
 		ConcretePerson.partnerMutually(personOne, personTwo);
 		assertSame(personOne.getPartner(), personTwo);
 		assertSame(personTwo.getPartner(), personOne);
@@ -22,16 +22,16 @@ public class ConcretePersonTest {
 		assertNull(testPerson.getName());
 		assertNull(testPerson.getPair());
 		assertNull(testPerson.getPartner());
-		
-		testPerson = new ConcretePerson("Jacob");
+
+		testPerson = (ConcretePerson) ConcretePerson.newPerson("Jacob");
 		assertEquals(testPerson.getName(), "Jacob");
 	}
-	
+
 	@Test
 	public void testSetName() {
 		ConcretePerson testPerson = (ConcretePerson) new ConcretePerson().setName("Jacob");
 		assertEquals(testPerson.getName(), "Jacob");
-		
+
 		testPerson.setName(null);
 		assertNull(testPerson.getName());
 	}
@@ -41,12 +41,12 @@ public class ConcretePersonTest {
 		ConcretePerson testPersonPartner = new ConcretePerson();
 		ConcretePerson testPerson = (ConcretePerson) new ConcretePerson().setPartner(testPersonPartner);
 		assertSame(testPerson.getPartner(), testPersonPartner);
-		
+
 		ConcretePerson testPairPerson = new ConcretePerson();
 		testPerson.setPair(testPairPerson);
 		testPerson.setPartner(testPairPerson);
 		assertNotSame(testPerson.getPartner(), testPairPerson);
-		
+
 		testPerson.setPartner(null);
 		assertNull(testPerson.getPartner());
 	}
@@ -56,40 +56,40 @@ public class ConcretePersonTest {
 		ConcretePerson testPersonPair = new ConcretePerson();
 		ConcretePerson testPerson = (ConcretePerson) new ConcretePerson().setPair(testPersonPair);
 		assertSame(testPerson.getPair(), testPersonPair);
-		
+
 		ConcretePerson testPartnerPerson = new ConcretePerson();
 		testPerson.setPartner(testPartnerPerson);
 		testPerson.setPair(testPartnerPerson);
 		assertNotSame(testPerson.getPair(), testPartnerPerson);
 	}
-	
+
 	@Test
 	public void testGetWasPaired() {
 		ConcretePerson testPerson = new ConcretePerson();
 		assertFalse(testPerson.getWasPaired());
-		
+
 		ConcretePerson testOtherPerson = new ConcretePerson();
 		testOtherPerson.setPair(testPerson);
 		assertTrue(testPerson.getWasPaired());
 	}
-	
+
 	@Test
 	public void testIsPaired() {
 		ConcretePerson testPerson = new ConcretePerson();
 		assertFalse(testPerson.isPaired());
-		
+
 		ConcretePerson testPairPerson = new ConcretePerson();
 		testPerson.setPair(testPairPerson);
 		assertTrue(testPerson.isPaired());
 	}
-	
+
 	@Test
 	public void testToString() {
-		ConcretePerson testPerson = new ConcretePerson("Jacob");
-		ConcretePerson testPersonPartner = new ConcretePerson("Mary");
-		ConcretePerson testPersonPair = new ConcretePerson("Joe");
+		ConcretePerson testPerson = (ConcretePerson) ConcretePerson.newPerson("Jacob");
+		ConcretePerson testPersonPartner = (ConcretePerson) ConcretePerson.newPerson("Mary");
+		ConcretePerson testPersonPair = (ConcretePerson) ConcretePerson.newPerson("Joe");
 		testPerson.setPartner(testPersonPartner).setPair(testPersonPair);
-		
+
 		assertEquals(testPerson.toString(), "Jacob is assigned Joe");
 	}
 
