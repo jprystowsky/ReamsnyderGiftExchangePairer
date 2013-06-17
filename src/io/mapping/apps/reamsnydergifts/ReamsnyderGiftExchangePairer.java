21c20590ba434d7a80284189e44138190bd259ee
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2013-06-17 11:38:40 -0400

    Making the main class responsible for saying who is getting paired.

--- src/io/mapping/apps/reamsnydergifts/ReamsnyderGiftExchangePairer.java
+++ src/io/mapping/apps/reamsnydergifts/ReamsnyderGiftExchangePairer.java
@@ -2,7 +2,9 @@ package io.mapping.apps.reamsnydergifts;
 
 import io.mapping.apps.reamsnydergifts.interfaces.PairingAlgorithm;
 import io.mapping.apps.reamsnydergifts.interfaces.Person;
+import io.mapping.apps.reamsnydergifts.models.ConcretePerson;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -10,10 +12,48 @@ public class ReamsnyderGiftExchangePairer {
 
 	public static void main(String[] args) {
 		PairingAlgorithm pairer = HeuristicPairingAlgorithm.newPairer();
-		List<Person> people = pairer.doPairing();
 
+		pairer.setPersons(getGiftPersons());
+
+		List<Person> people = pairer.doPairing();
 		for (Person p : people) {
 			System.out.println(p);
 		}
+	}
+
+	/**
+	 * Creates a static list of persons to be gift-paired.
+	 *
+	 * @return a list of Person objects
+	 */
+	private static List<Person> getGiftPersons() {
+		Person jacob = ConcretePerson.newPerson("Jacob");
+		Person mary = ConcretePerson.newPerson("Mary");
+		ConcretePerson.partnerMutually(jacob, mary);
+
+		Person joe = ConcretePerson.newPerson("Joe");
+		Person ashley = ConcretePerson.newPerson("Ashley");
+		ConcretePerson.partnerMutually(joe, ashley);
+
+		Person jake = ConcretePerson.newPerson("Jake");
+		Person megan = ConcretePerson.newPerson("Megan");
+		ConcretePerson.partnerMutually(jake, megan);
+
+		Person lee = ConcretePerson.newPerson("Lee");
+		Person kristin = ConcretePerson.newPerson("Kristin");
+		ConcretePerson.partnerMutually(lee, kristin);
+
+		List<Person> people = new ArrayList<Person>();
+
+		people.add(jacob);
+		people.add(mary);
+		people.add(joe);
+		people.add(ashley);
+		people.add(jake);
+		people.add(megan);
+		people.add(lee);
+		people.add(kristin);
+
+		return people;
 	}
 }
