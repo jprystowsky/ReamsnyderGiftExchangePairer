ac0565d1bd4424e94f809c77b0c4d83dd3360dad
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2013-06-17 10:26:09 -0400

    Fully documenting the Person interface.

--- src/io/mapping/apps/reamsnydergifts/Person.java
+++ src/io/mapping/apps/reamsnydergifts/Person.java
@@ -1,20 +1,71 @@
 package io.mapping.apps.reamsnydergifts;
 
-
+/**
+ * Provides an interface representing a physical person that can have a partner (i.e., romantically), which is a person
+ * who should be excluded from gift-giving consideration (as a person will buy his or her partner gifts already), as
+ * well as a pair, which is the person to whom this person will give a gift.
+ * <p/>
+ * A set of such persons will form a DCG (Directed Cyclic Graph).
+ */
 public interface Person {
-    public String getName();
+	/**
+	 * Returns a String representing the name of a person.
+	 *
+	 * @return the name of the person
+	 */
+	public String getName();
 
-    public Person setName(String n);
+	/**
+	 * Sets the name of this person to a new value.
+	 *
+	 * @param n the new name to assign
+	 * @return this person
+	 */
+	public Person setName(String n);
 
-    public Person getPartner();
+	/**
+	 * Returns the partner of this person.
+	 *
+	 * @return the person's partner (person who this person should not have as a pair)
+	 */
+	public Person getPartner();
 
-    public Person setPartner(Person p);
+	/**
+	 * Sets the partner of this person to a new person
+	 *
+	 * @param p the person who should be this person's partner
+	 * @return this person
+	 */
+	public Person setPartner(Person p);
 
-    public Person getPair();
+	/**
+	 * Returns the pair of this person, i.e., the person to whom this person will give a gift.
+	 *
+	 * @return the pair of this person
+	 */
+	public Person getPair();
 
-    public Person setPair(Person p);
+	/**
+	 * Sets the pair of this person to a new person
+	 *
+	 * @param p the person to whom this person should give a gift
+	 * @return this person
+	 */
+	public Person setPair(Person p);
 
-    public Boolean getWasPaired();
+	/**
+	 * Returns whether or not this person was paired to another person, i.e., whether or not there exists a person p1 in
+	 * the graph of persons such that getPair(p1) == this.
+	 *
+	 * @return a Boolean object describing whether or not this person is another person's pair person
+	 */
+	public Boolean getWasPaired();
 
-    public Person setWasPaired(Boolean b);
+	/**
+	 * Sets whether or not this person is another person's pair.
+	 *
+	 * @param b the value to indicate the pair status of this person with respect to another person
+	 * @return a Boolean describing whether or not this person is another person's pair
+	 */
+	public Person setWasPaired(Boolean b);
 }
