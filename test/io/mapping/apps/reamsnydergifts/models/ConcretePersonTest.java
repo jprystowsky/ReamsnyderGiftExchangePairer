77bb3a84255321471005a91432bf4c2db2f49969
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2013-06-17 11:16:54 -0400

    Moved ConcretePersonTest.

--- test/io/mapping/apps/reamsnydergifts/models/ConcretePersonTest.java
+++ test/io/mapping/apps/reamsnydergifts/models/ConcretePersonTest.java
@@ -1,8 +1,8 @@
-package io.mapping.apps.reamsnydergifts;
-
-import static org.junit.Assert.*;
+package io.mapping.apps.reamsnydergifts.models;
 
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class ConcretePersonTest {
 
